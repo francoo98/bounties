0ab81ebd5a61b7209429908eae0ca4f9645097ad
Franco Ayala <franco.a98@outlook.com>
2022-06-20 18:15:23 -0300

    Added a creation date field to Bounty

--- project/bounties/models.py
+++ project/bounties/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 class Bounty(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField(max_length=1000)
     reward = models.PositiveSmallIntegerField()
+    creation_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         constraints = [
-            models.CheckConstraint(check=models.Q(reward__dt=0), name='reward_dt_0'),
+            models.CheckConstraint(check=models.Q(reward__range=(1, 500)), name='reward_range'),
         ]
