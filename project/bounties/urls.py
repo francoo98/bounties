6806a159322a71f029c41388f1180a4488e30ba8
Franco Ayala <franco.a98@outlook.com>
2022-06-22 17:25:55 -0300

    Made the create_bounty view class based

--- project/bounties/urls.py
+++ project/bounties/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import DeleteBounty, RecentBounties, ViewBounty, create_bounty
+from .views import DeleteBounty, RecentBounties, ViewBounty, CreateBounty
 
 app_name = 'bounties'
 
 urlpatterns = [
     path('', RecentBounties.as_view(), name="recent_bounties"),
     path('<int:pk>', ViewBounty.as_view(), name="view_bounty"),
-    path('create', create_bounty, name="create_bounty"),
+    path('create', CreateBounty.as_view(), name="create_bounty"),
     path('delete/<int:pk>', DeleteBounty.as_view(), name="delete_bounty"),
 ]
--- project/bounties/views.py
+++ project/bounties/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from bounties.models import Bounty
 
@@ -20,10 +21,7 @@ class DeleteBounty(generic.DeleteView):
     model = Bounty
     success_url = reverse_lazy('bounties:recent_bounties')
 
-def create_bounty(request):
-    if request.method == 'GET':
-        return render(request, 'bounties/create_bounty.html')
-    if request.method == 'POST':
-        new_bounty = Bounty(title = request.POST["title"], description = request.POST["description"], reward = request.POST["reward"])
-        new_bounty.save()
-        return redirect(new_bounty)
+class CreateBounty(LoginRequiredMixin, generic.CreateView):
+    model = Bounty
+    fields = ['title', 'description', 'reward']
+    template_name = 'bounties/create_bounty.html'
