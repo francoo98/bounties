665ff6f6ab4ba7c23ea04ed3c7b03ffa9b7a3b86
Franco Ayala <franco.a98@outlook.com>
2022-06-28 12:26:13 -0300

    Added test for creator null constraint

--- project/bounties/tests.py
+++ project/bounties/tests.py
@@ -5,13 +5,15 @@ from django.test import Client, TestCase
 from .models import Bounty
 from django.contrib.auth.models import User
 
+
 class BountyDeleteViewTestCase(TestCase):
 
     def setUp(self):
         User.objects.create(username="franco")
         user = User.objects.get(username="franco")
-        Bounty.objects.create(title="Test bounty", reward=300, description="something", creator=user)
-    
+        Bounty.objects.create(title="Test bounty", reward=300,
+                              description="something", creator=user)
+
     def test_bounty_delete_view(self):
         u = User.objects.get(username="franco")
         c = Client()
@@ -21,6 +23,7 @@ class BountyDeleteViewTestCase(TestCase):
         bounty = Bounty.objects.get(title="Test bounty")
         self.assertEquals(bounty.status, Bounty.DELETED)
 
+
 class BountyCreateTestCase(TestCase):
 
     def setUp(self):
@@ -29,4 +32,9 @@ class BountyCreateTestCase(TestCase):
 
     def test_bounty_reward_constraint(self):
         user = User.objects.get(username="franco")
-        self.assertRaises(django.db.utils.IntegrityError, Bounty.objects.create, title="Test bounty", reward=-300, description="something", creator=user)
+        self.assertRaises(django.db.utils.IntegrityError, Bounty.objects.create,
+                          title="Test bounty", reward=-300, description="something", creator=user)
+
+    def test_user_not_null(self):
+        self.assertRaises(django.db.utils.IntegrityError, Bounty.objects.create,
+                          title="Test bounty", reward=-300, description="something")
