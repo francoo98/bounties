64801d2026f529c8a9ef6262003aa0d7c8948be7
Franco Ayala <franco.a98@outlook.com>
2022-06-28 12:05:11 -0300

    Added test for bounty delete view

--- project/bounties/tests.py
+++ project/bounties/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from pydoc import describe
+from django.test import Client, TestCase
+from .models import Bounty
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class BountyDeleteViewTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(username="franco")
+        user = User.objects.get(username="franco")
+        Bounty.objects.create(title="Test bounty", reward=300, description="something", creator=user)
+    
+    def test_bounty_delete_view(self):
+        u = User.objects.get(username="franco")
+        c = Client()
+        c.force_login(u)
+        bounty = Bounty.objects.get(title="Test bounty")
+        r = c.post(f"/bounty/delete/{bounty.id}", {"pk": bounty.id})
+        bounty = Bounty.objects.get(title="Test bounty")
+        self.assertEquals(bounty.status, Bounty.DELETED)
