1ef7e0c4349bfb8e980c92603eda087b42e833d3
Franco Ayala <franco.a98@outlook.com>
2022-07-11 12:05:43 -0300

    Cleaned some code

--- project/bounties_api/urls.py
+++ project/bounties_api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -14,12 +13,3 @@ router.register(r'solution', SolutionViewSet, basename='solutions')
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-"""
-urlpatterns = [
-    path('', BountyList.as_view(), name='bounties_list'),
-    path('<int:pk>', BountyDetail.as_view(), name='bounty_detail'),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-"""
--- project/bounties_api/views.py
+++ project/bounties_api/views.py
@@ -1,6 +1,4 @@
-from rest_framework import generics, permissions, status
-from rest_framework.reverse import reverse
-from rest_framework.decorators import api_view
+from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework import viewsets
 
@@ -8,16 +6,6 @@ from rest_framework import viewsets
 from bounties.models import Bounty, Solution
 from .serializers import BountySerializer, SolutionSerializer
 from .permissions import IsOwnerOrReadOnly
-
-
-"""
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('users_api:users_list', request=request, format=format),
-        'bounties': reverse('bounties_list', request=request, format=format)
-    })
-"""
 
 class BountyViewSet(viewsets.ModelViewSet):
     queryset = Bounty.objects.all()
@@ -48,30 +36,3 @@ class SolutionViewSet(viewsets.ModelViewSet):
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-class BountyList(generics.ListCreateAPIView):
-    """
-    List all bounties (GET) or create a new one (POST)
-    """
-
-    queryset = Bounty.objects.all()
-    serializer_class = BountySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(creator=self.request.user)
-
-
-class BountyDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve, update or delete a Bounty instance
-    """
-
-    queryset = Bounty.objects.all()
-    serializer_class = BountySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
-
-    def perform_destroy(self, bounty):
-        bounty.status = Bounty.DELETED
-        bounty.save()
