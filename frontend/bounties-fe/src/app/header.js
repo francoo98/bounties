0768250b43a92cddc793e5187809e1aaaf33a8d0
Franco Ayala <franco.a98@outlook.com>
2024-07-29 19:52:56 -0300

    Added a link to Home to the header

--- frontend/bounties-fe/src/app/header.js
+++ frontend/bounties-fe/src/app/header.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Header() {
-    return (
-      <div id={styles.header}>
-      <h1 id={styles.title}>Software bounties</h1>
+  return (
+    <div id={styles.header}>
+      <Link href="/">
+        <h1 id={styles.title}>Software bounties</h1>
+      </Link>
       <button id={styles.loginButton}>Login</button>
-      </div>
-    );
+    </div>
+  );
 }
