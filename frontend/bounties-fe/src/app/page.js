509c968fdf0fc052da823013c0ea71f49588b458
Franco Ayala <franco.a98@outlook.com>
2024-07-29 19:52:31 -0300

    Moved the header to the layout

--- frontend/bounties-fe/src/app/page.js
+++ frontend/bounties-fe/src/app/page.js
@@ -1,11 +1,7 @@
-import Header from "./header";
 import BountiesList from "./bounties-list";
 
 export default function Home() {
   return (
-    <>
-    <Header></Header>
     <BountiesList></BountiesList>
-    </>
   );
 }
